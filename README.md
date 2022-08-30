8365a73b9b711e4c3dd49e4939ec4dbee6a64e75
I306454 <jiabin.zheng01@sap.com>
2022-08-30 17:29:39 +0800

    Setup project

---
+++ README.md
@@ -0,0 +1,6 @@
+# executable-jar
+
+创建可执行 jar 包的样板 project，包含
+
+* idea 创建可执行 jar
+* maven shade plugin 创建可执行 jar
---
+++ src/main/java/jk/Main.java
@@ -0,0 +1,11 @@
+package jk;
+
+public class Main {
+    public static void main(String[] args) {
+        if(args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        } else {
+            System.out.println("Hello world");
+        }
+    }
+}
